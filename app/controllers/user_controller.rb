bad0a93bed25168e02e62d5d4370e17d72c2ac76
Mustapha <ait.mus06@gmail.com>
2020-03-12 12:18:25 +0100

    add method in create user action

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,5 +3,17 @@ class UserController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: "OK", status: :created
+    else
+      new_pseudo = ""
+      loop do
+        new_pseudo = Array.new(3){[*"A".."Z"].sample}.join
+        break unless User.exists?(name: new_pseudo)
+      end
+      User.create(name:new_pseudo)
+      render json: "Ce pseudo est déjà prit ou ne respecte pas les règles (3 caractères max, tout en majuscule, pas de chiffre). Nous vous proposons celui-ci : #{new_pseudo}", status: :created
+    end
   end
 end
