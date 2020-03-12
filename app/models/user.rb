2a5c75e5dd49129fcb4eda059de05d08715898e4
Mustapha <ait.mus06@gmail.com>
2020-03-12 12:31:06 +0100

    add 'presence: true' validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, uniqueness: true
+  validates :name,presence: true, uniqueness: true
   validates_format_of :name, :with => /\A[a-z]+\z/i
   validates :name, length: { is: 3 }
 end
